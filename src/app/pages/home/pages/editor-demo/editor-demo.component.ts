f82019b4fcecc70c90c682bec3317813125f7d25
laixiangran <laixiangran@163.com>
2016-12-21 19:20:37 +0800

    去掉没用的属性

--- src/app/pages/home/pages/editor-demo/editor-demo.component.ts
+++ src/app/pages/home/pages/editor-demo/editor-demo.component.ts
@@ -9,8 +9,6 @@ export class EditorDemoComponent implements OnInit {
 
     @ViewChild('editor') editor: EssenceNg2EditorComponent;
 
-    e_width: number = 800;
-    e_height: number = 300;
     model_text: string = '<p style="font-weight: bold;"><a href="http://ueditor.baidu.com/website/index.html" target="_blank" title="去UEditor官网">UEditor Component for Angular2</a></p>';
 
     constructor () {
