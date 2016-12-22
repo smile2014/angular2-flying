d403d723bb2553970f8b8ccac525bc17a3cbdf42
laixiangran <laixiangran@163.com>
2016-12-22 13:51:58 +0800

    测试自定义打印

--- src/app/pages/home/pages/print-demo/print-demo.component.ts
+++ src/app/pages/home/pages/print-demo/print-demo.component.ts
@@ -1,47 +1,56 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {EssenceNg2PrintComponent} from "../../../../components/essence-ng2-print";
 
 @Component({
-	templateUrl: './print-demo.component.html',
-	styleUrls: ['./print-demo.component.scss']
+    templateUrl: './print-demo.component.html',
+    styleUrls: ['./print-demo.component.scss']
 })
 export class PrintDemoComponent implements OnInit {
-	datas: any[];
-	printCSS: string[];
-	printStyle: string;
+    @ViewChild('print1') printComponent: EssenceNg2PrintComponent;
+    showHead: boolean = true;
+    datas: any[];
+    printCSS: string[];
+    printStyle: string;
 
-	constructor () {
-		this.datas = [
-			{
-				"firstName": 'Mark',
-				"lastName": 'Otto',
-				"userName": '@mdo'
-			},
-			{
-				"firstName": 'Jacob',
-				"lastName": 'Thornton',
-				"userName": '@fat'
-			},
-			{
-				"firstName": 'Larry',
-				"lastName": 'the Bird',
-				"userName": '@twitter'
-			}
-		];
+    constructor () {
+        this.datas = [
+            {
+                "firstName": 'Mark',
+                "lastName": 'Otto',
+                "userName": '@mdo'
+            },
+            {
+                "firstName": 'Jacob',
+                "lastName": 'Thornton',
+                "userName": '@fat'
+            },
+            {
+                "firstName": 'Larry',
+                "lastName": 'the Bird',
+                "userName": '@twitter'
+            }
+        ];
 
-		this.printCSS = ['http://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css'];
-		this.printStyle =
-			`
+        this.printCSS = ['http://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css'];
+        this.printStyle =
+            `
 			th, td {
 				color: red !important;
 			}
 			`;
-	}
+    }
 
-	ngOnInit () {
-	}
+    ngOnInit () {
+    }
 
-	printComplete () {
-		console.log('打印完成！');
-	}
+    printComplete () {
+        console.log('打印完成！');
+        this.showHead = true;
+    }
+
+    customPrint () {
+        this.showHead = false;
+        this.printComponent.print();
+    }
 
 }
